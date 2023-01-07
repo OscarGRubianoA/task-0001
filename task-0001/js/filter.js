b7b8880555f446d9aa8348ec66623cd3f5a9619f
BetoCochas <oscargio86@hotmail.com>
2023-01-06 22:57:16 -0500

    'filter'

---
+++ task-0001/js/filter.js
@@ -0,0 +1,23 @@
+
+function filtros() {
+    if (arrayfiltrado.length != 0) {
+        imprimir(arrayfiltrado, 'main__container');
+    } else {
+        imprimir(arraysinfiltrar, 'main__container');
+    }
+}
+filtros()
+function categoryfilter(datacheck) {
+    let checksactive = Array.from(document.querySelectorAll("input[type='checkbox']"))
+    let checkactive = checksactive.filter(checkbox => checkbox.checked)
+    
+    let checkboxValue = checkactive.map(checkbox => checkbox.value)
+   
+    let fcategory = datacheck.filter(dat => checkboxValue.includes(dat.category));
+    
+    if (fcategory.length) {
+        return fcategory
+    }
+    return datacheck
+
+}
