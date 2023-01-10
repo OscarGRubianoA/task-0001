cf5d8d38e58fffc4b3c707634756a2cc2f2c9926
BetoCochas <oscargio86@hotmail.com>
2023-01-10 04:24:45 -0500

    'index'

--- task-0001/js/index.js
+++ task-0001/js/index.js
@@ -1,19 +1,67 @@
-const contenedor = document.getElementById("main__container");
+const $cards = document.getElementById("main__container");
+const $search = document.getElementById("search");
+const $categorys = document.getElementById("menu");
 
-function createCard(element, array) {
+// Async
+let eventos;
+// let favoritos =  []
+fetch("https://mindhub-xj03.onrender.com/api/amazing")
+  .then(data => data.json())
+  .then(data => {
+    eventos = data.events;
+    console.log(eventos)
+    crearCheckbox(eventos, $categorys)
+    imprimirCards(eventos, $cards)
+    $search.addEventListener('keyup', filtrar)
+    $categorys.addEventListener('change', filtrar)
+  })
+  .catch(error => console.log(error));
 
-    for (let event of array.events) {
-        element.innerHTML += `<section>
-        <img src='${event.image}' alt="${event.name}">
-        <h5>${event.name}</h5>
-        <p>${event.description}</p>
-        <div>
-        <p>${event.price}</p>
-        <button>Read more...</button>
-        </div>
-        </section>`
-    
-    }
+// DOM | CheckBox
+function crearCheckbox(eventos, contenedor) {
+  let fn = eventos => eventos.category
+  let categorias = new Set(eventos.map(fn))
+  categorias.forEach(par => {
+    contenedor.innerHTML += `
+    <label class="form-check-label" for="${par}">
+    <input class="form-check-input" value="${par}" type="checkbox" role="switch" id="${par}">${par}
+    </label>
+    `
+  })
 }
 
-createCard(contenedor, data);
+function crearCard(eventos) {
+  let div = document.createElement('DIV')
+  div.classList = 'card cardD'
+  div.style = 'width: 14rem'
+  div.innerHTML += `
+    <img src="${eventos.image}" class="card-img-top" alt="${eventos.name}"/> 
+    <div class="card-body">
+    <h5 class="card-title">${eventos.name}</h5>
+    <p class="card-text">${eventos.description}</p>
+    <a class="btn btn-dark">U$D ${eventos.price}</a>
+    <a href="./pages/onlycard.html?id=${eventos.id}" class="btn btn-danger">See more</a>
+    </div> 
+    `
+  return div
+}
+function imprimirCards(eventos, contenedor) {
+  contenedor.innerHTML = ''
+  if (eventos.length > 0) {
+    let fragment = document.createDocumentFragment()
+    eventos.forEach(eventos => fragment.appendChild(crearCard(eventos)))
+    contenedor.appendChild(fragment)
+  }
+  else {
+    contenedor.innerHTML = `<h2>Sin coincidencias...</h2>`
+  }
+}
+
+function filtrar() {
+  let checked = [...document.querySelectorAll('input[type="checkbox"]:checked')].map(ele => ele.value)
+  let filtradosPorCategoria = eventos.filter(eventos => checked.includes(eventos.category) || checked.length == 0)
+  console.log(filtradosPorCategoria)
+  let filtradosPorSearch = filtradosPorCategoria.filter(iteracion =>
+     iteracion.name.toLowerCase().includes($search.value.toLowerCase()))
+  imprimirCards(filtradosPorSearch, $cards)
+}
